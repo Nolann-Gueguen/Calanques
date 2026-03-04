06282736de63704e70ab4fb3d33110862903694e
Nolann-Gueguen <nolann.gueguen14@gmail.com>
2026-03-04 11:16:26 +0100

    Activité fonctionnelle

--- app/src/main/java/com/example/calanques/MainActivity.kt
+++ app/src/main/java/com/example/calanques/MainActivity.kt
@@ -16,6 +16,7 @@ import com.example.calanques.ui.theme.CalanquesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             CalanquesTheme {
