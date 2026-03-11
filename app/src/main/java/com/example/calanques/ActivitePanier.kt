e9c52df7d1d0f186488d0dc994c920cc50de5b20
Maxime-Leu <leu.maxime27@gmail.com>
2026-03-11 09:07:28 +0100

    Modification du fichier ActivitePanier afin d'accueillir les donnees de la BDD.

--- app/src/main/java/com/example/calanques/ActivitePanier.kt
+++ app/src/main/java/com/example/calanques/ActivitePanier.kt
@@ -1,11 +1,14 @@
 package com.example.calanques
 
 data class ActivitePanier(
-    val titre: String,
-    val date: String,
-    val personnes: Int,
-    val prixParPersonne: Int
+    val reservation_id: Int,
+    val activite_id: Int,
+    val titre: String, // Jointure
+    val date_activite: String,
+    val heure_activite: String,
+    val nb_participants: Int,
+    val prixParPersonne: Int // Jointure
 ) {
     val montant: Int
-        get() = personnes * prixParPersonne
+        get() = nb_participants * prixParPersonne
 }
