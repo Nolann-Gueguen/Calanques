e34f82850e41a957436927b2fb603eb8bf68d3a0
Valent1n2-alcAla <alcalavalentin55@gmail.com>
2026-03-11 09:54:40 +0100

    Correction des conflits

--- app/src/main/java/com/example/calanques/ApiService.kt
+++ app/src/main/java/com/example/calanques/ApiService.kt
@@ -1,8 +1,14 @@
 package com.example.calanques
 
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface ApiService {
@@ -16,4 +22,24 @@ interface ApiService {
 
     @DELETE("api/reservations/{reservation_id}")
     suspend fun deleteReservation(@Path("reservation_id") id: Int): Response<Unit>
+
+    @POST("api/auth/signup")
+    suspend fun registerUser(@Body request: UserCreateRequest): ApiResponse
+
+    @FormUrlEncoded
+    @POST("api/auth/login")
+    suspend fun login(
+        @Field("username") username: String, // FastAPI utilise 'username' pour recevoir l'e-mail
+        @Field("password") password: String
+    ): TokenResponse
+
+    // La route pour récupérer le profil avec le pass VIP
+    @GET("api/users/me")
+    suspend fun getMe(@Header("Authorization") token: String): UserResponse
+
+    @PUT("api/users/me")
+    suspend fun updateMe(
+        @Header("Authorization") token: String,
+        @Body request: UserUpdateRequest
+    ): UserResponse
 }
